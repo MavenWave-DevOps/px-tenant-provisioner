0ae289a5999360d6b27f12c0f49828ed98d5f43d
tony-mw <81250444+tony-mw@users.noreply.github.com>
2022-10-24 12:23:37 -0600

    rm wlid

--- hack/wlid_root.sh
+++ hack/wlid_root.sh
@@ -2,7 +2,7 @@
 
 PROJECT_ID=tony-sandbox-308422
 SA_NAME=sa-workload-px-tnt-prov
-ROLES=("roles/iam.securityAdmin" "roles/container.admin" "roles/iam.serviceAccountTokenCreator" "roles/iam.serviceAccountAdmin" )
+ROLES=("roles/iam.securityAdmin" "roles/container.admin" "roles/iam.serviceAccountTokenCreator" "roles/secretmanager.admin" "roles/iam.serviceAccountAdmin" )
 K8S_NAMESPACE=default
 K8S_SA=sa-workload-px-tnt-prov
 
